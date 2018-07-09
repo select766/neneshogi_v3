f0fcf490ef1743525fd1115d33569b1fcccd04d0
select766 <select766@outlook.jp>
2018-07-09 21:12:07 +0900

    training_sessionを使った学習(WIP)

--- neneshogi/neneshogi/packed_sfen_data_source.py
+++ neneshogi/neneshogi/packed_sfen_data_source.py
@@ -6,7 +6,7 @@ import os
 import struct
 import numpy as np
 import cntk as C
-from cntk.io import UserMinibatchSource, StreamInformation, MinibatchData
+from cntk.io import UserMinibatchSource, StreamInformation, MinibatchData, FULL_DATA_SWEEP, INFINITELY_REPEAT
 from neneshogi_cpp import DNNConverter
 
 
@@ -14,7 +14,7 @@ class PackedSfenDataSource(UserMinibatchSource):
     RECORD_SIZE = 40
 
     def __init__(self, path: str, count: Optional[int] = None, skip: int = 0,
-                 format_board: int = 0, format_move: int = 0):
+                 format_board: int = 0, format_move: int = 0, max_samples: int = INFINITELY_REPEAT):
         fsize = os.path.getsize(path)
         total_record_count = fsize // PackedSfenDataSource.RECORD_SIZE
         if count is None:
@@ -23,6 +23,9 @@ class PackedSfenDataSource(UserMinibatchSource):
         self.skip = skip
         self.count = count
         self.record_idx = 0
+        self.max_samples = max_samples
+        self.total_generated_samples = 0
+        self.no_next_sweep = False
         self._seek_to_head()
 
         self._cvt = DNNConverter(format_board, format_move)
@@ -53,21 +56,30 @@ class PackedSfenDataSource(UserMinibatchSource):
         assert number_of_workers == 1 and worker_rank == 0, "multi worker not supported"
         boards = []
         moves = []
-        sweep_end = True
-        while len(boards) < num_samples:
+        sweep_end = False
+        while len(boards) < num_samples and self.total_generated_samples < self.max_samples and (
+                not self.no_next_sweep):
             board, move = self._read_record()
             boards.append(board)
             moves.append(move)
 
             self.record_idx += 1
+            self.total_generated_samples += 1
             if self.record_idx >= self.count:
                 self._seek_to_head()
                 self.record_idx = 0
                 sweep_end = True
+                if self.max_samples == FULL_DATA_SWEEP:
+                    self.no_next_sweep = True
+                    break
+
+        n_items = len(boards)
+        if n_items == 0:
+            # When the maximum number of epochs/samples is exhausted, the return value is an empty dict.
+            return {}
 
         board_data = C.Value(batch=np.array(boards), device=device)
         move_data = C.Value(batch=np.array(moves), device=device)
-        n_items = len(boards)
         res = {
             self.board_info: MinibatchData(board_data, n_items, n_items, sweep_end),
             self.move_info: MinibatchData(move_data, n_items, n_items, sweep_end),
@@ -75,9 +87,14 @@ class PackedSfenDataSource(UserMinibatchSource):
         return res
 
     def get_checkpoint_state(self):
-        return {"record_idx": self.record_idx}
+        return {"record_idx": self.record_idx,
+                "total_generated_samples": self.total_generated_samples,
+                "no_next_sweep": self.no_next_sweep}
 
     def restore_from_checkpoint(self, state):
         self.record_idx = state["record_idx"]
+        self.total_generated_samples = state["total_generated_samples"]
+        self.no_next_sweep = state["no_next_sweep"]
         assert isinstance(self.record_idx, int)
+        assert isinstance(self.total_generated_samples, int)
         self._file.seek((self.skip + self.record_idx) * PackedSfenDataSource.RECORD_SIZE)
