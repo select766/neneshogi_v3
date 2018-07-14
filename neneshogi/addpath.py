054a0b78c8e3cef5267e13dd6f25e9ff235e821b
select766 <select766@outlook.jp>
2018-07-15 08:20:21 +0900

    sse42でのaddpath.py実行を可能にする

--- neneshogi/addpath.py
+++ neneshogi/addpath.py
@@ -1,12 +1,27 @@
 """
 Register neneshogi C++ python module to import path
-Add __init__.py to module directory
+Copy __init__.py[i] from avx2 directory to specified arch
 """
 
 import os
+import sys
 import site
+import distutils.file_util
 
-target_dir = os.path.abspath(__file__ + "/../../build/pymodule/avx2")
+arch = sys.argv[1] if len(sys.argv) == 2 else "avx2"
+
+target_dir = os.path.abspath(__file__ + "/../../build/pymodule/" + arch)
+init_src_dir = os.path.abspath(__file__ + "/../../build/pymodule/avx2")
+pyd_path = os.path.join(target_dir, "neneshogi_cpp.pyd")
+if not os.path.exists(pyd_path):
+    print(pyd_path + " does not exist. Abort.")
+    sys.exit(1)
+
+if arch != "avx2":
+    for fn in ["__init__.py", "__init__.pyi"]:
+        distutils.file_util.copy_file(os.path.join(init_src_dir, fn),
+                                      os.path.join(target_dir, fn),
+                                      verbose=True)
 sdir = site.getsitepackages()[-1]
 pth_path = os.path.join(sdir, "neneshogi_cpp.pth")
 print("Creating pth file as " + pth_path)
