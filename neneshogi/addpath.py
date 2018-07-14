d61ed17e0a1df7ceffdb826cb6073308e175c92d
select766 <select766@outlook.jp>
2018-07-15 08:22:14 +0900

    パス修正

--- neneshogi/addpath.py
+++ neneshogi/addpath.py
@@ -12,15 +12,15 @@ arch = sys.argv[1] if len(sys.argv) == 2 else "avx2"
 
 target_dir = os.path.abspath(__file__ + "/../../build/pymodule/" + arch)
 init_src_dir = os.path.abspath(__file__ + "/../../build/pymodule/avx2")
-pyd_path = os.path.join(target_dir, "neneshogi_cpp.pyd")
+pyd_path = os.path.join(target_dir, "neneshogi_cpp", "neneshogi_cpp.pyd")
 if not os.path.exists(pyd_path):
     print(pyd_path + " does not exist. Abort.")
     sys.exit(1)
 
 if arch != "avx2":
     for fn in ["__init__.py", "__init__.pyi"]:
-        distutils.file_util.copy_file(os.path.join(init_src_dir, fn),
-                                      os.path.join(target_dir, fn),
+        distutils.file_util.copy_file(os.path.join(init_src_dir, "neneshogi_cpp", fn),
+                                      os.path.join(target_dir, "neneshogi_cpp", fn),
                                       verbose=True)
 sdir = site.getsitepackages()[-1]
 pth_path = os.path.join(sdir, "neneshogi_cpp.pth")
