9799e2c3131f8cd6eadac233edac476e902c6bf6
select766 <select766@outlook.jp>
2018-07-20 06:17:26 +0900

    学習状態保存

---
+++ neneshogi/neneshogi/train_manager.py
@@ -0,0 +1,87 @@
+from collections import defaultdict
+from typing import Dict
+
+
+class TrainManager:
+    next_action: str
+    main_criterion: str
+    last_val_main_criterion: float
+    lr: float
+    lr_reduce_ratio: float
+    min_lr: float
+    quit_reason: str
+    epoch_size: int
+    batch_size: int
+    trained_samples: int
+    val_frequency: int
+    first_diverge_check_size: int
+    diverge_criterion: Dict[str, float]
+    average_mean_criterions: Dict[str, float]  # train errorの移動平均
+
+    def __init__(self, epoch_size: int, batch_size: int, val_frequency: int,
+                 initial_lr: float, min_lr: float, first_diverge_check_size: int,
+                 diverge_criterion: Dict[str, float]):
+        self.next_action = "train"
+        self.main_criterion = "policy_cle"
+        self.last_val_main_criterion = None
+        self.lr = initial_lr
+        self.lr_reduce_ratio = 2.0
+        self.min_lr = min_lr
+        self.quit_reason = None
+        self.epoch_size = epoch_size
+        self.batch_size = batch_size
+        self.trained_samples = 0
+        self.val_frequency = val_frequency
+        self.first_diverge_check_size = first_diverge_check_size
+        self.diverge_criterion = diverge_criterion
+        self.average_mean_criterions = defaultdict(float)
+
+    def get_next_action(self):
+        if self.quit_reason is not None:
+            print(f"quit: {self.quit_reason}")
+            return {"action": "quit", "reason": self.quit_reason}
+        return {"action": self.next_action, "lr": self.lr}
+
+    def _check_diverge(self, mean_criterions: Dict[str, float]) -> bool:
+        """
+        発散基準を満たすかどうかチェックする。
+        指定されたエラー率が閾値以上なら発散したとみなす。
+        :param mean_criterions:
+        :return:
+        """
+        for key, thres in self.diverge_criterion.items():
+            if mean_criterions[key] > thres:
+                return True
+        return False
+
+    def put_train_result(self, mean_criterions: Dict[str, float]):
+        last_val_cycle = self.trained_samples // self.val_frequency
+        self.trained_samples += self.batch_size
+        for k, v in mean_criterions.items():
+            self.average_mean_criterions[k] = self.average_mean_criterions[k] * 0.99 + v * 0.01
+        if self.trained_samples >= self.first_diverge_check_size:
+            # 発散チェック
+            # サンプルによっては特別に悪い場合があるので、移動平均で判定
+            if self._check_diverge(self.average_mean_criterions):
+                self.quit_reason = "diverge"
+        do_val = self.trained_samples // self.val_frequency > last_val_cycle
+        if do_val:
+            print("average_mean_criterions", self.average_mean_criterions)
+            self.next_action = "val"
+
+    def put_val_result(self, mean_criterions: Dict[str, float]):
+        main_score = mean_criterions[self.main_criterion]
+        if self.last_val_main_criterion is not None:
+            improve_ratio = 1.0 - main_score / self.last_val_main_criterion
+            print(f"val score improvement: {improve_ratio}")
+            if improve_ratio < 0.01:
+                # 改善がほとんどない
+                # lrを下げる
+                print("reducing lr")
+                self.lr /= self.lr_reduce_ratio
+                if self.lr < self.min_lr:
+                    # 学習終了
+                    self.quit_reason = "lr_below_min"
+
+        self.last_val_main_criterion = main_score
+        self.next_action = "train"
--- neneshogi/neneshogi/train_proto3.py
+++ neneshogi/neneshogi/train_proto3.py
@@ -19,7 +19,7 @@ CNTK 2.5.1時点で、今のところ自前で書かないと2つのロスを別
 import os
 import argparse
 from collections import defaultdict
-from typing import Dict
+import pickle
 
 import numpy as np
 
@@ -27,102 +27,47 @@ import cntk as C
 from neneshogi import util
 from neneshogi.packed_sfen_data_source import PackedSfenDataSource
 from neneshogi import models
+from neneshogi.train_manager import TrainManager
 
 
-class TrainManager:
-    next_action: str
-    main_criterion: str
-    last_val_main_criterion: float
-    lr: float
-    lr_reduce_ratio: float
-    min_lr: float
-    quit_reason: str
-    epoch_size: int
-    batch_size: int
-    trained_samples: int
-    val_frequency: int
-    first_diverge_check_size: int
-    diverge_criterion: Dict[str, float]
-    average_mean_criterions: Dict[str, float]  # train errorの移動平均
+def save_checkpoint(workdir, model_config, train_manager, data_sources, model):
+    cp_dir = os.path.join(workdir, "checkpoint")
+    os.makedirs(cp_dir, exist_ok=True)
+    model.save(os.path.join(cp_dir,
+                            "nene_{}_{}.cmf".format(model_config["format_board"], model_config["format_move"])))
+    status = {"train_manager": train_manager,
+              "ds_train_state": data_sources["train"].get_checkpoint_state(),
+              "ds_test_state": data_sources["test"].get_checkpoint_state()}
+    with open(os.path.join(cp_dir, "status.bin"), "wb") as f:
+        pickle.dump(status, f, protocol=pickle.HIGHEST_PROTOCOL)
 
-    def __init__(self, epoch_size: int, batch_size: int, val_frequency: int,
-                 initial_lr: float, min_lr: float, first_diverge_check_size: int,
-                 diverge_criterion: Dict[str, float]):
-        self.next_action = "train"
-        self.main_criterion = "policy_cle"
-        self.last_val_main_criterion = None
-        self.lr = initial_lr
-        self.lr_reduce_ratio = 2.0
-        self.min_lr = min_lr
-        self.quit_reason = None
-        self.epoch_size = epoch_size
-        self.batch_size = batch_size
-        self.trained_samples = 0
-        self.val_frequency = val_frequency
-        self.first_diverge_check_size = first_diverge_check_size
-        self.diverge_criterion = diverge_criterion
-        self.average_mean_criterions = defaultdict(float)
 
-    def get_next_action(self):
-        if self.quit_reason is not None:
-            print(f"quit: {self.quit_reason}")
-            return {"action": "quit", "reason": self.quit_reason}
-        return {"action": self.next_action, "lr": self.lr}
-
-    def _check_diverge(self, mean_criterions: Dict[str, float]) -> bool:
-        """
-        発散基準を満たすかどうかチェックする。
-        指定されたエラー率が閾値以上なら発散したとみなす。
-        :param mean_criterions:
-        :return:
-        """
-        for key, thres in self.diverge_criterion.items():
-            if mean_criterions[key] > thres:
-                return True
-        return False
-
-    def put_train_result(self, mean_criterions: Dict[str, float]):
-        last_val_cycle = self.trained_samples // self.val_frequency
-        self.trained_samples += self.batch_size
-        for k, v in mean_criterions.items():
-            self.average_mean_criterions[k] = self.average_mean_criterions[k] * 0.99 + v * 0.01
-        if self.trained_samples >= self.first_diverge_check_size:
-            # 発散チェック
-            # サンプルによっては特別に悪い場合があるので、移動平均で判定
-            if self._check_diverge(self.average_mean_criterions):
-                self.quit_reason = "diverge"
-        do_val = self.trained_samples // self.val_frequency > last_val_cycle
-        if do_val:
-            print("average_mean_criterions", self.average_mean_criterions)
-            self.next_action = "val"
-
-    def put_val_result(self, mean_criterions: Dict[str, float]):
-        main_score = mean_criterions[self.main_criterion]
-        if self.last_val_main_criterion is not None:
-            improve_ratio = 1.0 - main_score / self.last_val_main_criterion
-            print(f"val score improvement: {improve_ratio}")
-            if improve_ratio < 0.01:
-                # 改善がほとんどない
-                # lrを下げる
-                print("reducing lr")
-                self.lr /= self.lr_reduce_ratio
-                if self.lr < self.min_lr:
-                    # 学習終了
-                    self.quit_reason = "lr_below_min"
-
-        self.last_val_main_criterion = main_score
-        self.next_action = "train"
+def load_checkpoint(workdir, model_config, data_sources):
+    cp_dir = os.path.join(workdir, "checkpoint")
+    with open(os.path.join(cp_dir, "status.bin"), "rb") as f:
+        status = pickle.load(f)
+    data_sources["train"].restore_from_checkpoint(status["ds_train_state"])
+    data_sources["test"].restore_from_checkpoint(status["ds_test_state"])
+    return status["train_manager"]
 
 
 # Create the network.
-def create_shogi_model(board_shape, move_dim, model_config):
+def create_shogi_model(board_shape, move_dim, model_config, restore_path=None):
     # Input variables denoting the features and label data
     feature_var = C.input_variable(board_shape)
     policy_var = C.input_variable(move_dim)
     value_var = C.input_variable(1)
 
-    model_function = getattr(models, model_config["name"])
-    policy, value = model_function(feature_var, board_shape, move_dim, **model_config["kwargs"])
+    if restore_path:
+        print(f"restoring from {restore_path}")
+        model_function = C.load_model(restore_path)
+        combined = model_function(feature_var)
+        policy = combined[0]
+        value = combined[1]
+    else:
+        model_function = getattr(models, model_config["name"])
+        policy, value = model_function(feature_var, board_shape, move_dim, **model_config["kwargs"])
+
 
     # loss and metric
     loss_policy_ce = C.cross_entropy_with_softmax(policy, policy_var)
@@ -159,7 +104,13 @@ def shogi_train_and_eval(solver_config, model_config, workdir, restore):
                                        format_move=model_config["format_move"],
                                        max_samples=C.io.INFINITELY_REPEAT)
 
-    network = create_shogi_model(train_source.board_shape, train_source.move_dim, model_config)
+    restore_path = None
+    if restore:
+        restore_path = os.path.join(workdir, "checkpoint",
+                                    "nene_{}_{}.cmf".format(model_config["format_board"], model_config["format_move"]))
+        if not os.path.exists(restore_path):
+            restore_path = None
+    network = create_shogi_model(train_source.board_shape, train_source.move_dim, model_config, restore_path)
     lr_schedule = C.learning_parameter_schedule(1e-4)
     mm_schedule = C.learners.momentum_schedule(0.9)
     learner = C.learners.momentum_sgd(network['output'].parameters, lr_schedule, mm_schedule, unit_gain=False,
@@ -176,10 +127,13 @@ def shogi_train_and_eval(solver_config, model_config, workdir, restore):
     trainer = C.Trainer(network['output'], (network['total_error'], criterion), [learner])
 
     val_frequency = solver_config["val_frequency"]
-    manager = TrainManager(epoch_size=epoch_size, batch_size=batch_size, val_frequency=val_frequency,
-                           initial_lr=solver_config["lr"], min_lr=solver_config["lr"] / 1000,
-                           first_diverge_check_size=val_frequency,
-                           diverge_criterion={"policy_cle": 0.95, "value_ce": 0.6})
+    if restore_path:
+        manager = load_checkpoint(workdir, model_config, {"train": train_source, "test": test_source})
+    else:
+        manager = TrainManager(epoch_size=epoch_size, batch_size=batch_size, val_frequency=val_frequency,
+                               initial_lr=solver_config["lr"], min_lr=solver_config["lr"] / 1000,
+                               first_diverge_check_size=val_frequency,
+                               diverge_criterion={"policy_cle": 0.95, "value_ce": 0.6})
     last_lr = None
     i = 0
     while True:
@@ -221,6 +175,9 @@ def shogi_train_and_eval(solver_config, model_config, workdir, restore):
                 mean_criterions[k] = v / n_validated_samples
             print("val_criterions", mean_criterions)
             manager.put_val_result(mean_criterions)
+            print("saving")
+            save_checkpoint(workdir, model_config, manager, {"train": train_source, "test": test_source},
+                            network["output"])
 
 
 if __name__ == '__main__':
